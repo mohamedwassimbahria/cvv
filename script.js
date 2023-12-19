b878f5befbaf6272c078990b3112f12462fd4f33
mohamedwassimbahria <mohamedwassim.bahrialasgher@esprit.tn>
2023-12-19 12:13:01 +0100

    Ajouter les modification

--- script.js
+++ script.js
@@ -6,40 +6,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const nomError = document.getElementById('nomError');
     const emailError = document.getElementById('emailError');
     const messageError = document.getElementById('messageError');
-    const successMessage = document.getElementById('successMessage'); // Add this line
+    const successMessage = document.getElementById('successMessage'); 
 
     contactForm.addEventListener('submit', function (event) {
         console.log('Formulaire soumis.');
 
-        // Validation logic
         if (!ValidNom(nomInput.value)) {
             event.preventDefault();
             nomError.textContent = 'Veuillez saisir un nom valide (lettres seulement)';
-        } else {
-            nomError.textContent = '';
         }
-
         if (!ValidEmail(emailInput.value)) {
             event.preventDefault();
             emailError.textContent = 'Veuillez saisir une adresse e-mail valide';
-        } else {
-            emailError.textContent = '';
         }
 
         if (!ValidMessage(messageInput.value)) {
             event.preventDefault();
             messageError.textContent = 'Veuillez saisir un message';
-        } else {
-            messageError.textContent = '';
         }
-
-        // Check if all fields are valid before showing success message
+        if (!ValidNom(nomInput.value) && !ValidEmail(emailInput.value) && !ValidMessage(messageInput.value)){
+            globalError.textContent = 'Veuillez corriger les erreurs dans le formulaire.';
+        }
         if (ValidNom(nomInput.value) && ValidEmail(emailInput.value) && ValidMessage(messageInput.value)) {
             console.log('Success message:', successMessage.textContent);
             successMessage.textContent = 'Votre formulaire a été soumis avec succès!';
             setTimeout(function () {
                 window.location.href = "index.html";
-            }, 2000);
+            }, 10000);
         } else {
             successMessage.textContent = '';
         }
